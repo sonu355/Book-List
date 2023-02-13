049da2fe29bdcfe0871b2d6b34ae6fe5fa97431f
harshal <harshalc562@gmail.com>
2023-02-13 20:24:42 +0530

    Done!

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // book class : represent a book
-class Book{
-    constructor(title, author, isbn){
+class Book {
+    constructor(title, author, isbn) {
         this.title = title
         this.author = author
         this.isbn = isbn
@@ -8,13 +8,13 @@ class Book{
 }
 
 function displayBooks() {
-    const storeBooks = getBooks()    
+    const storeBooks = getBooks()
     console.log(storeBooks)
-    for(let i = 0; i < storeBooks.length; i++){
+    for (let i = 0; i < storeBooks.length; i++) {
         const book = storeBooks[i]
         console.log(storeBooks[i])
         addBookToList(book)
-    } 
+    }
 
 }
 
@@ -29,22 +29,22 @@ function addBookToList(book) {
     `
     list.appendChild(row)
 }
-  
-function deleteBooks(el){
-    if(el.classList.contains('delete')){
+
+function deleteBooks(el) {
+    if (el.classList.contains('delete')) {
         el.parentElement.parentElement.remove()
     }
 }
 
-function showAlert(message, className){
+function showAlert(message, className) {
     const div = document.createElement('div')
     div.className = `alert alert-${className}`
     div.appendChild(document.createTextNode(message))
     const container = document.querySelector('.container')
     const form = document.querySelector('#book-form')
-    container.insertBefore(div,form)
+    container.insertBefore(div, form)
     //vanish in 3 sec
-    setTimeout(() => document.querySelector('.alert').remove(),3000)
+    setTimeout(() => document.querySelector('.alert').remove(), 3000)
 }
 
 function clearField() {
@@ -55,12 +55,10 @@ function clearField() {
 
 function getBooks() {
     let books;
-    if(localStorage.getItem('books') === null) {
-        console.log(books,'boosks')
+    if (localStorage.getItem(books) === null) {
         books = []
-
-    }else {
-        books = localStorage.getItem('books')
+    } else {
+        books = localStorage.getItem(books)
         console.log(books)
     }
     return books;
@@ -69,24 +67,24 @@ function getBooks() {
 function addBook(book) {
     const books = getBooks();
     books.push(book)
-    localStorage.setItem('books',JSON.stringify('books'))
+    localStorage.setItem('books', JSON.stringify('books'))
 }
 
 function removeBook(isbn) {
     const books = getBooks()
     books.forEach((book, index) => {
-        if(book.isbn === isbn){
-            book.splice(index,1)
+        if (book.isbn === isbn) {
+            book.splice(index, 1)
         }
     })
     localStorage.setItem('books', JSON.stringify(books))
 }
 
 //Event : display books
-document.addEventListener('DOMContentLoaded',displayBooks())
+document.addEventListener('DOMContentLoaded', displayBooks())
 
 //Add A book
-document.querySelector('#book-form').addEventListener('submit',(e) => {
+document.querySelector('#book-form').addEventListener('submit', (e) => {
     e.preventDefault()
     //get form values
     const title = document.querySelector('#title').value
@@ -94,29 +92,31 @@ document.querySelector('#book-form').addEventListener('submit',(e) => {
     const isbn = document.querySelector('#isbn').value
 
     //validation
-    if(title === '' || author ==='' || isbn ===''){
-        showAlert('please fill the details','danger')
-    }else{
-        const book = new Book(title,author,isbn)
+    if (title === '' || author === '' || isbn === '') {
+        showAlert('please fill the details', 'danger')
+    } else {
+        const book = new Book(title, author, isbn)
         console.log(book)
-    //Add book to UI
-    addBookToList(book)
+        //Add book to UI
+        addBookToList(book)
 
-    //add book to store
-    addBook()
+        //add book to store
+        addBook()
 
-    //shiw success message
-    showAlert('book added successfully','success')
+        //shiw success message
+        showAlert('book added successfully', 'success')
 
-    //clear fields
-    clearField()
+        //clear fields
+        clearField()
     }
-    
+
 })
 // delete the fields
-document.querySelector('#book-list').addEventListener('click',(e) => {
+document.querySelector('#book-list').addEventListener('click', (e) => {
     deleteBooks(e.target)
-
-    showAlert('book removed','info')
+     //remove book from store
+    removeBook(e.target.parentElement.previousElementSibiling.textContent)
+    
+    showAlert('book removed', 'info')
 
 })
