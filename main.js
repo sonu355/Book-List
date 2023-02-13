f6e6fe38a2a47a85c2ca5b03640a2a93161a15f2
harshal <harshalc562@gmail.com>
2023-02-13 16:19:18 +0530

    Remove Classs

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 // book class : represent a book
 class Book{
-    constructor(book, author, isbn){
-        this.book = book
+    constructor(title, author, isbn){
+        this.title = title
         this.author = author
         this.isbn = isbn
     }
 }
-
-//class UI : handles UI
-class UI{
-    static displayBooks() {
-        const StoreBooks = [
+function displayBooks() {
+    const storeBooks = [
             {
                 title:'Book one',
                 author:'John doe',
@@ -22,30 +19,37 @@ class UI{
                 isbn:'#8768'
             }
         ];
-        const books = StoreBooks
-        books.forEach((book) => UI.addBookToList())
+        for(let i = 0; i < storeBooks.length; i++){
+            const book = storeBooks[i]
+            console.log(storeBooks[1])
+            const list = document.querySelector('#book-list')
+            const row = document.createElement('tr')
+            row.innerHTML = `
+            <td>${book.title}</td>i
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="#" class="btn btn-danger btn-sm delete">Delete</a></td>`;
+            list.appendChild(row);
+        }
     }
-    static addBookToList(book){
+    function addBookToList() {
         const list = document.querySelector('#book-list')
-
-        const row = document.createElement('tr')
-        row.innerHTML = `
-        <td>${book.title}</td>
-        <td>${book.author}</td>
-        <td>${book.isbn}</td>
-        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
-        `
-        list.appendChild(row);
+        consar row = 
     }
-}
 
 //Event : display books
 document.addEventListener('DOMContentLoaded', UI.displayBooks)
 
 //Add A book
 document.querySelector('#book-form').addEventListener('submit',(e) => {
+    e.preventDefault()
     //get form values
     const title = document.querySelector('#title').value
     const author = document.querySelector('#author').value
     const isbn = document.querySelector('#isbn').value
+
+    const book = new Book(title,author,isbn)
+    console.log(book)
+    //Add book to UI
+    addBookToList(book)
 })
